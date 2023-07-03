6c5aed374ed7df933a477756365215ac1664b92c
dlrkdmf <dlrkdmf46@naver.com>
2023-07-03 17:44:22 +0900

    test2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "WEB TEST"
+    return "Terraform CI/CD Project"
 
 if __name__  == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
